1e75b9f1f2b8b8324db052599b929bd4d7413a56
Anubhavanandrai <anubhavanand.rai@gmail.com>
2024-09-08 00:25:46 +0530

    5th

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [], // Ensure 'react-router-dom' is not listed here
+      external: ['react-router-dom'], // Ensure 'react-router-dom' is not listed here
     },
   },
 });
